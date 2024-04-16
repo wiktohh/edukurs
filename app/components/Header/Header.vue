866e619681cfa995d13db5ba2a9fd5064b7be15d
wiktohh <wiktor.rzeznicki01@gmail.com>
2024-04-16 10:35:47 +0200

    add footer and header

---
+++ app/components/Header/Header.vue
@@ -0,0 +1,19 @@
+import ChangeThemeButton from '../ChangeThemeButton.vue';
+<template>
+  <v-app-bar>
+    <v-toolbar-title class="title">Edukurs</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn to="/home">Home</v-btn>
+    <v-btn to="/courses">All courses</v-btn>
+    <v-btn to="/admin">Administration panel</v-btn>
+    <v-btn to="/auth?variant=register">Logout</v-btn>
+    <ChangeThemeButton />
+  </v-app-bar>
+</template>
+<style lang="scss">
+.title {
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  font-size: 1.5rem;
+}
+</style>
