28fde3635584000858d7d8f9f17a25471882c192
wiktohh <wiktor.rzeznicki01@gmail.com>
2024-04-16 10:36:15 +0200

    add auth page

--- app/pages/auth.vue
+++ app/pages/auth.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="main">
+    <v-btn @click="backToHomePage" variant="plain" class="back-button">
+      <v-icon class="arrow-icon">mdi-arrow-left</v-icon>
+      Powrót
+    </v-btn>
+    <ChangeThemeButton class="theme-button" />
     <h1>{{ variant === "register" ? "Rejestracja" : "Logowanie" }}</h1>
     <div className="container">
       <v-form @submit.prevent>
@@ -56,12 +61,17 @@ import {
 
 type Variant = "login" | "register";
 const route = useRoute();
+const router = useRouter();
 const variant = ref<Variant>((route.query.variant as Variant) || "login");
 
 const firstName = ref("");
 const lastName = ref("");
 const email = ref("");
 const password = ref("");
+
+function backToHomePage() {
+  router.push({ path: "/" });
+}
 
 watch(
   () => route.query.variant,
@@ -79,6 +89,21 @@ watch(
   justify-content: center;
   align-items: center;
   gap: 1rem;
+
+  .back-button {
+    position: absolute;
+    top: 1rem;
+    left: 1rem;
+    .arrow-icon {
+      margin-right: 0.5rem;
+    }
+  }
+  .theme-button {
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+  }
+
   .h1 {
     text-align: center;
   }
