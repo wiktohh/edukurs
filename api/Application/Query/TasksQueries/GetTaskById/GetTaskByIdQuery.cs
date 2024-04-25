d0bebaf36c112462ed17c210ccf34e74419ef705
JakubWaj <jw303269@student.polsl.pl>
2024-04-25 16:42:04 +0200

    Getting task by id

---
+++ api/Application/Query/TasksQueries/GetTaskById/GetTaskByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using Application.DTO;
+using MediatR;
+
+namespace Application.Query.TasksQueries.GetTaskById;
+
+public class GetTaskByIdQuery : IRequest<TaskDto>
+{
+    public Guid TaskId { get; set; }
+}
---
+++ api/Application/Query/TasksQueries/GetTaskById/GetTaskByIdQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using Application.DTO;
+using Application.Exceptions;
+using Domain.Repositories;
+using MediatR;
+
+namespace Application.Query.TasksQueries.GetTaskById;
+
+public class GetTaskByIdQueryHandler : IRequestHandler<GetTaskByIdQuery, TaskDto>
+{
+    private readonly ITaskRepository _repository;
+
+    public GetTaskByIdQueryHandler(ITaskRepository repository)
+    {
+        _repository = repository;
+    }
+    
+    public async Task<TaskDto> Handle(GetTaskByIdQuery request, CancellationToken cancellationToken)
+    {
+        var repTask = await _repository.GetRepTaskAsync(request.TaskId);
+        if (repTask == null)
+        {
+            throw new NotFoundException("Task not found");
+        }
+        return repTask.AsDto();
+    }
+}
