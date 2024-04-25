29c3359f645a9507639f8c51682dfb56a48ff6d3
JakubWaj <jw303269@student.polsl.pl>
2024-04-25 16:41:48 +0200

    creating new task

---
+++ api/Application/Command/Tasks/CreateTask/CreateTaskCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Application.Command.Tasks.CreateTask;
+
+public class CreateTaskCommand : IRequest
+{
+    public Guid UserId { get; set; }
+    public Guid RepTaskId { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime Deadline { get; set; }
+    public Guid RepositoryId { get; set; }
+}
---
+++ api/Application/Command/Tasks/CreateTask/CreateTaskCommandHandler.cs
@@ -0,0 +1,51 @@
+﻿using Application.Exceptions;
+using Domain.Entities;
+using Domain.Repositories;
+using Domain.ValueObjects.Repository;
+using Domain.ValueObjects.RepTask;
+using MediatR;
+
+namespace Application.Command.Tasks.CreateTask;
+
+public class CreateTaskCommandHandler : IRequestHandler<CreateTaskCommand>
+{
+    private readonly ITaskRepository _taskRepository;
+    private readonly IRepRepository _repRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CreateTaskCommandHandler(ITaskRepository taskRepository,IRepRepository repRepository,IUnitOfWork unitOfWork)
+    {
+        _taskRepository = taskRepository;
+        _repRepository = repRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task Handle(CreateTaskCommand request, CancellationToken cancellationToken)
+    {
+        var repository = await _repRepository.GetRepositoryByIdAsync(request.RepositoryId);
+        if (repository is null)
+        {
+            throw new NotFoundException("Repository not found");
+        }   
+        if (repository.OwnerId != request.UserId)
+        {
+            throw new UnauthorazedException("You are not the owner of this repository");
+        }
+        RepTaskId repTaskId = new RepTaskId(request.RepTaskId);
+        Title title = new Title(request.Title);
+        Description description = new Description(request.Description);
+        Deadline deadline = new Deadline(request.Deadline);
+        RepositoryId repositoryId = new RepositoryId(request.RepositoryId);
+
+        var task = new RepTask
+        {
+            Id = repTaskId,
+            Title = title,
+            Description = description,
+            Deadline = deadline,
+            RepositoryId = repositoryId
+        };
+        await _taskRepository.AddRepTaskAsync(task);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+    }
+}
