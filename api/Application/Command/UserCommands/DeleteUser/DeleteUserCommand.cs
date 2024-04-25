d2251d003f9fdc509ea07391208fbcf463091eaf
JakubWaj <jw303269@student.polsl.pl>
2024-04-25 16:41:41 +0200

    Deleting user from repository

---
+++ api/Application/Command/UserCommands/DeleteUser/DeleteUserCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Application.Command.UserCommands.DeleteUser;
+
+public class DeleteUserCommand : IRequest
+{
+    public Guid Id { get; set; }
+}
---
+++ api/Application/Command/UserCommands/DeleteUser/DeleteUserCommandQuery.cs
@@ -0,0 +1,32 @@
+﻿using Application.Exceptions;
+using Domain.Repositories;
+using MediatR;
+
+namespace Application.Command.UserCommands.DeleteUser;
+
+public class DeleteUserCommandQuery : IRequestHandler<DeleteUserCommand>
+{
+    private readonly IAccountRepository _repository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public DeleteUserCommandQuery(IAccountRepository repository, IUnitOfWork unitOfWork)
+    {
+        _repository = repository;
+        _unitOfWork = unitOfWork;
+    }
+    
+    public async Task Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+    {
+        var user = await _repository.GetByIdAsync(request.Id);
+        if (user == null)
+        {
+            throw new Exception("User not found");
+        }
+        _repository.DeleteAsync(user);
+        var result = await _unitOfWork.SaveChangesAsync(cancellationToken);
+        if (!result)
+        {
+            throw new SavingChangesException("user");
+        }
+    }
+}
